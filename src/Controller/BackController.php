13748660cb04033832c244849539c31c2618833e
Josep02 <josepferrer99@gmail.com>
2024-05-17 02:05:13 +0200

    Back updated

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\EventRepository;
-use App\Repository\MusicianRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,30 +14,6 @@ class BackController extends AbstractController
     {
         return $this->render('back/index.html.twig', [
             'controller_name' => 'BackController',
-        ]);
-    }
-
-    #[Route('/events', name: 'app_back_events')]
-    public function events(EventRepository $eventRepository): Response
-    {
-        $events = $eventRepository->findAll();
-        $activeEvents = $eventRepository->findBy(['state' => 'Active']);
-        $closedEvents = $eventRepository->findBy(['state' => 'Closed']);
-
-        return $this->render('back/back_events.html.twig', [
-            'events' => $events,
-            'activeEvents' => $activeEvents,
-            'closedEvents' => $closedEvents,
-        ]);
-    }
-
-    #[Route('/users', name: 'app_back_users')]
-    public function users(MusicianRepository $musicianRepository): Response
-    {
-        $users = $musicianRepository->findAll();
-
-        return $this->render('back/back_users.html.twig', [
-            'users' => $users
         ]);
     }
 }
