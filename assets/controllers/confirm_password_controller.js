b20a24954e4c1e8b8e228a2d32a41f253fedb60e
Josep02 <josepferrer99@gmail.com>
2024-06-12 11:26:41 +0200

    Compare passwords apply

---
+++ assets/controllers/confirm_password_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    static targets = ["password", "confirmPassword"];
+
+    connect() {
+        console.log("ConfirmPasswordController connected");
+        this.confirmPasswordTarget.addEventListener('input', () => this.confirm());
+        this.passwordTarget.addEventListener('input', () => this.confirm());
+    }
+
+    confirm() {
+        const password = this.passwordTarget.value;
+        const confirmPassword = this.confirmPasswordTarget.value;
+
+        console.log(`Password: ${password}, Confirm Password: ${confirmPassword}`);
+
+        this.passwordTarget.classList.remove('border-success', 'border-danger');
+        this.confirmPasswordTarget.classList.remove('border-success', 'border-danger');
+
+        if (password && confirmPassword) {
+            if (password === confirmPassword) {
+                this.confirmPasswordTarget.classList.add('border-success');
+                this.passwordTarget.classList.add('border-success');
+            } else {
+                this.confirmPasswordTarget.classList.add('border-danger');
+                this.passwordTarget.classList.add('border-danger');
+            }
+        }
+    }
+}
